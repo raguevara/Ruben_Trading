6326047deefa103642c035654a08e082f81789a8
Ruben Guevara <raguevara2@hotmail.com>
2021-01-05 10:35:55 -0500

    Importar Acciones de apple, baba, fb, nvda, msft

--- Acciones.py
+++ Acciones.py
@@ -19,6 +19,24 @@ from datetime import datetime
 ts = TimeSeries(key='FH69CNJPTEAVXKHO',output_format='pandas')
 
 #importar datos
+
+#APPLE
 aapl, meta_data = ts.get_intraday(symbol='AAPL',interval='1min', outputsize='full')
 aapl = aapl.rename(columns = {'1. open':'open','2. high':'high','3. low':'low','4. close':'close','5. volume':'volume'})
+
+#ALIBABA
+baba, meta_data = ts.get_intraday(symbol='BABA',interval='1min', outputsize='full')
+baba = baba.rename(columns = {'1. open':'open','2. high':'high','3. low':'low','4. close':'close','5. volume':'volume'})
 print(aapl.head())
+
+#FACEBOOK
+fb, meta_data = ts.get_intraday(symbol='FB',interval='1min', outputsize='full')
+fb = fb.rename(columns = {'1. open':'open','2. high':'high','3. low':'low','4. close':'close','5. volume':'volume'})
+
+#NVIDIA
+nvda, meta_data = ts.get_intraday(symbol='NVDA',interval='1min', outputsize='full')
+nvda = nvda.rename(columns = {'1. open':'open','2. high':'high','3. low':'low','4. close':'close','5. volume':'volume'})
+
+#MICROSOFT
+msft, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
+msft = msft.rename(columns = {'1. open':'open','2. high':'high','3. low':'low','4. close':'close','5. volume':'volume'})
